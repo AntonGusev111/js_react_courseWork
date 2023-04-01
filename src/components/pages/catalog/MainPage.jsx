f3dc69b0cdd6f5b930a4af9d8b740f67a6561e58
GusevADresume <resumeGusevAD@gmail.com>
2023-04-01 23:56:20 +0300

    v2

--- src/components/pages/catalog/MainPage.jsx
+++ src/components/pages/catalog/MainPage.jsx
@@ -3,14 +3,21 @@ import { CatalogList } from "./CatalogList";
 import { Hits } from "../hits/Hits";
 import { LoadButton } from "./LoadButton";
 import { Categories } from "./Categories";
+import { useState } from "react";
 import goods from "../../testVariables";
 
 function MainPage() {
+  const [state, setState] = useState(11);
   const catigories = [
     { id: 11, title: "All" },
     { id: 12, title: "Мужская обувь" },
     { id: 13, title: "Женская обувь" },
   ];
+
+  const handleChose = (e) => {
+    setState(e.target.id);
+  };
+
   return (
     <>
       <section className="top-sales">
@@ -18,17 +25,19 @@ function MainPage() {
       </section>
       <section className="catalog">
         <h2 className="text-center">Каталог</h2>
-        {catigories.map((element) => {
-          return (
-            <li
-              onClick={handleChose}
-              key={element.id}
-              className="nav-item-categories"
-            >
-              <Categories element={element} select={state} />
-            </li>
-          );
-        })}
+        <ul className="catalog-categories nav justify-content-center">
+          {catigories.map((element) => {
+            return (
+              <li
+                onClick={handleChose}
+                key={element.id}
+                className="nav-item-categories"
+              >
+                <Categories element={element} select={state} />
+              </li>
+            );
+          })}
+        </ul>
         <CatalogList goods={goods} />
         <LoadButton />
       </section>
